4f1a85dc5b20687193bff745456ec26c2c27f79d
Sky4git <sky4git@users.noreply.github.com>
2025-05-28 12:05:21 +1000

    Bump version to 1.0 and implement API Gateway with Lambda integration for restaurant deals

--- src/main/java/com/myorg/ServerlessJavaExampleStack.java
+++ src/main/java/com/myorg/ServerlessJavaExampleStack.java
@@ -3,8 +3,18 @@ package com.myorg;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.events.targets.LambdaFunction;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.lambda.Code;
+
+import java.util.List;
+
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.services.apigatewayv2.AddRoutesOptions;
+import software.amazon.awscdk.services.apigatewayv2.HttpApi;
+import software.amazon.awscdk.services.apigatewayv2.HttpMethod;
+import software.amazon.awscdk.aws_apigatewayv2_integrations.HttpLambdaIntegration;
 
 public class ServerlessJavaExampleStack extends Stack {
     public ServerlessJavaExampleStack(final Construct scope, final String id) {
@@ -14,11 +24,33 @@ public class ServerlessJavaExampleStack extends Stack {
     public ServerlessJavaExampleStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        // Lambda for Task 1
+        // Write an API that returns a list of all the available restaurant deals
+        // that are active at a specified time of day
+        Function getRestaurantDealsByTime = Function.Builder.create(this, "GetRestaurantDealsByTime")
+                .functionName("GetRestaurantDealsByTime")
+                .runtime(Runtime.JAVA_21)
+                .code(Code.fromAsset("lambda/getRestaurantDealsByTime"))
+                .handler("com.myorg.Task1Handler::handleRequest")
+                .memorySize(128)
+                .timeout(Duration.seconds(30))
+                .build();
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "ServerlessJavaExampleQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        // Define HTTP API Gateway v2 and integrate with Lambda
+        HttpLambdaIntegration dealsIntegration = new HttpLambdaIntegration(
+                "DealsIntegration", getRestaurantDealsByTime);
+
+        // Create the HTTP API
+        HttpApi httpApi = HttpApi.Builder.create(this, "RestaurantDealsHttpApi")
+                .apiName("RestaurantDealsHttpApi")
+                .build();
+
+        // Add a route to the API Gateway for the `getRestaurantDealsByTime` Lambda
+        // function
+        httpApi.addRoutes(AddRoutesOptions.builder()
+                .path("/deals")
+                .methods(List.of(HttpMethod.GET))
+                .integration(dealsIntegration)
+                .build());
     }
 }
